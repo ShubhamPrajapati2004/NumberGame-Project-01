9b3e4a3559ae987d260a8e1b53ab8121f6a32aef
SHUBHAM PRAJAPATI <166904871+ShubhamPrajapati2004@users.noreply.github.com>
2024-08-24 01:15:06 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -43,7 +43,8 @@ public class NumberGame
                 }
                 System.out.println("That's It Buddy, I Hope you Enjoyed it");
                 System.out.println("Here is your Score: "+finals +" Thankyou");}
-        }}
+        }
+    }
             public static int getrandN(int min,int max)
     {
                 return (int)(Math.random()*(max-min+1)+min);
